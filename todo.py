29dbdba70f9877bf053de88bf65c0f01fb80a246
qayol1 <qayol@freemail.hu>
2016-10-10 13:44:23 +0200

    Add files via upload

---
+++ todo.py
@@ -0,0 +1,33 @@
+import sys
+
+todo = []
+mark = []
+
+length = len(sys.argv)
+
+def add():
+
+def show():
+
+def mark():
+
+def archive():
+
+
+if length < 2:
+    exit
+
+if length == 2 and sys.argv[1] == "add":
+    add() 
+
+if length == 2 and sys.argv[1] == "list":
+    show()  
+
+if length == 2 and sys.argv[1] == "mark":
+    mark()  
+
+if length == 2 and sys.argv[1] == "archive":
+    archive()  
+
+if length > 2 
+        print("Please insert a number!")
